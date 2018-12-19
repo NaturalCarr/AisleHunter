9b3b3e3522c265fbdcde576bb8d5710033c228ab
isayahc <isayah@DESKTOP-1E903EP.localdomain>
2018-12-19 16:16:34 -0500

    Updated database

---
+++ Files/Insert.sql
@@ -0,0 +1,271 @@
+INSERT INTO STORE (Name, Address, Zip, Sun_O, Sun_C, Mon_O, Mon_C, Tue_O, Tue_C, Wed_O, Wed_C, Thu_O, Thu_C, Fri_O, Fri_C, Sat_O, Sat_C) VALUES ('Meyer''s Super Foods' , '82-19 Dr. Brule Avenue', '11892', '900', '1900', '730', '2300', '730', '2300', '730', '2300', '730', '2300', '730', '2300', '730', '2300');
+INSERT INTO STORE (Name, Address, Zip, Sun_O, Sun_C, Mon_O, Mon_C, Tue_O, Tue_C, Wed_O, Wed_C, Thu_O, Thu_C, Fri_O, Fri_C, Sat_O, Sat_C) VALUES ('Irrational Foods' , '122 broad Avenue', '14822', '900', '1900', '730', '2300', '730', '2300', '730', '2300', '730', '2300', '730', '2300', '730', '2300');
+INSERT INTO STORE (Name, Address, Zip, Sun_O, Sun_C, Mon_O, Mon_C, Tue_O, Tue_C, Wed_O, Wed_C, Thu_O, Thu_C, Fri_O, Fri_C, Sat_O, Sat_C) VALUES ('Some Foods' , '824 Broad Road', '15948', '900', '1900', '730', '2300', '730', '2300', '730', '2300', '730', '2300', '730', '2300', '730', '2300');
+INSERT INTO STORE (Name, Address, Zip, Sun_O, Sun_C, Mon_O, Mon_C, Tue_O, Tue_C, Wed_O, Wed_C, Thu_O, Thu_C, Fri_O, Fri_C, Sat_O, Sat_C) VALUES ('Supermarket of James' , '1845 James Street', '16892', '900', '1900', '730', '2300', '730', '2300', '730', '2300', '730', '2300', '730', '2300', '730', '2300');
+INSERT INTO STORE (Name, Address, Zip, Sun_O, Sun_C, Mon_O, Mon_C, Tue_O, Tue_C, Wed_O, Wed_C, Thu_O, Thu_C, Fri_O, Fri_C, Sat_O, Sat_C) VALUES ('Munch Town' , '165 Dame Street', '15562', '900', '1900', '730', '2300', '730', '2300', '730', '2300', '730', '2300', '730', '2300', '730', '2300');
+
+INSERT INTO AISLE (Number, StoreID) VALUES (1, 1);
+INSERT INTO AISLE (Number, StoreID) VALUES (2, 1);
+INSERT INTO AISLE (Number, StoreID) VALUES (3, 1);
+INSERT INTO AISLE (Number, StoreID) VALUES (4, 1);
+INSERT INTO AISLE (Number, StoreID) VALUES (5, 1);
+INSERT INTO AISLE (Number, StoreID) VALUES (6, 1);
+INSERT INTO AISLE (Number, StoreID) VALUES (7, 1);
+INSERT INTO AISLE (Number, StoreID) VALUES (8, 1);
+INSERT INTO AISLE (Number, StoreID) VALUES (1, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (2, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (3, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (4, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (5, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (6, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (7, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (8, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (9, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (10, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (11, 2);
+INSERT INTO AISLE (Number, StoreID) VALUES (1, 3);
+INSERT INTO AISLE (Number, StoreID) VALUES (2, 3);
+INSERT INTO AISLE (Number, StoreID) VALUES (3, 3);
+INSERT INTO AISLE (Number, StoreID) VALUES (4, 3);
+INSERT INTO AISLE (Number, StoreID) VALUES (5, 3);
+INSERT INTO AISLE (Number, StoreID) VALUES (6, 3);
+INSERT INTO AISLE (Number, StoreID) VALUES (7, 3);
+INSERT INTO AISLE (Number, StoreID) VALUES (8, 3);
+INSERT INTO AISLE (Number, StoreID) VALUES (9, 3);
+INSERT INTO AISLE (Number, StoreID) VALUES (1, 4);
+INSERT INTO AISLE (Number, StoreID) VALUES (2, 4);
+INSERT INTO AISLE (Number, StoreID) VALUES (3, 4);
+INSERT INTO AISLE (Number, StoreID) VALUES (4, 4);
+INSERT INTO AISLE (Number, StoreID) VALUES (5, 4);
+INSERT INTO AISLE (Number, StoreID) VALUES (6, 4);
+INSERT INTO AISLE (Number, StoreID) VALUES (7, 4);
+
+INSERT INTO ITEM (Name) VALUES ('Bacon
+')
+INSERT INTO ITEM (Name) VALUES ('Eggs
+')
+INSERT INTO ITEM (Name) VALUES ('Water
+')
+INSERT INTO ITEM (Name) VALUES ('Milk
+')
+INSERT INTO ITEM (Name) VALUES ('Bread
+')
+INSERT INTO ITEM (Name) VALUES ('Cheese
+')
+INSERT INTO ITEM (Name) VALUES ('Cereal
+')
+INSERT INTO ITEM (Name) VALUES ('Flour
+')
+INSERT INTO ITEM (Name) VALUES ('Soda
+')
+INSERT INTO ITEM (Name) VALUES ('Pasta
+')
+INSERT INTO ITEM (Name) VALUES ('Sugar Honey Ice Tea
+')
+INSERT INTO ITEM (Name) VALUES ('Coffe Beans
+')
+INSERT INTO ITEM (Name) VALUES ('Vegan Chicken Nuggets
+')
+INSERT INTO ITEM (Name) VALUES ('That Loud gummi bites
+')
+INSERT INTO ITEM (Name) VALUES ('Broccoli
+')
+INSERT INTO ITEM (Name) VALUES ('Pasta
+')
+INSERT INTO ITEM (Name) VALUES ('Soup
+')
+INSERT INTO ITEM (Name) VALUES ('Lettuce
+')
+INSERT INTO ITEM (Name) VALUES ('Tomatoes
+')
+INSERT INTO ITEM (Name) VALUES ('Cupcakes
+')
+INSERT INTO ITEM (Name) VALUES ('Honey
+')
+INSERT INTO ITEM (Name) VALUES ('Mineral Water
+')
+INSERT INTO ITEM (Name) VALUES ('Carrotts
+')
+INSERT INTO ITEM (Name) VALUES ('Black Beans
+')
+INSERT INTO ITEM (Name) VALUES ('Rice
+')
+INSERT INTO ITEM (Name) VALUES ('Kidney Beans
+')
+INSERT INTO ITEM (Name) VALUES ('Peas
+')
+INSERT INTO ITEM (Name) VALUES ('Honey Mustard
+')
+INSERT INTO ITEM (Name) VALUES ('Taco Shells
+')
+INSERT INTO ITEM (Name) VALUES ('Whipped Cream
+')
+INSERT INTO ITEM (Name) VALUES ('Oranges
+')
+INSERT INTO ITEM (Name) VALUES ('Apples
+')
+INSERT INTO ITEM (Name) VALUES ('Tea
+')
+INSERT INTO ITEM (Name) VALUES ('Onion
+')
+INSERT INTO ITEM (Name) VALUES ('Jelly
+')
+INSERT INTO ITEM (Name) VALUES ('PeanutButter
+')
+INSERT INTO ITEM (Name) VALUES ('Tomato Sauce
+')
+INSERT INTO ITEM (Name) VALUES ('Coconut Oil
+')
+INSERT INTO ITEM (Name) VALUES ('Chicken
+')
+INSERT INTO ITEM (Name) VALUES ('Strawberries
+')
+INSERT INTO ITEM (Name) VALUES ('Cucumber
+')
+INSERT INTO ITEM (Name) VALUES ('Avocado
+')
+INSERT INTO ITEM (Name) VALUES ('Waffles
+')
+INSERT INTO ITEM (Name) VALUES ('Bacon
+')
+INSERT INTO ITEM (Name) VALUES ('Eggs
+')
+INSERT INTO ITEM (Name) VALUES ('Water
+')
+INSERT INTO ITEM (Name) VALUES ('Milk
+')
+INSERT INTO ITEM (Name) VALUES ('Bread
+')
+INSERT INTO ITEM (Name) VALUES ('Cheese
+')
+INSERT INTO ITEM (Name) VALUES ('Cereal
+')
+INSERT INTO ITEM (Name) VALUES ('Flour
+')
+INSERT INTO ITEM (Name) VALUES ('Soda
+')
+INSERT INTO ITEM (Name) VALUES ('Pasta
+')
+INSERT INTO ITEM (Name) VALUES ('Sugar Honey Ice Tea
+')
+INSERT INTO ITEM (Name) VALUES ('Coffe Beans
+')
+INSERT INTO ITEM (Name) VALUES ('Vegan Chicken Nuggets
+')
+INSERT INTO ITEM (Name) VALUES ('That Loud gummi bites
+')
+INSERT INTO ITEM (Name) VALUES ('Broccoli
+')
+INSERT INTO ITEM (Name) VALUES ('Pasta
+')
+INSERT INTO ITEM (Name) VALUES ('Soup
+')
+INSERT INTO ITEM (Name) VALUES ('Lettuce
+')
+INSERT INTO ITEM (Name) VALUES ('Tomatoes
+')
+INSERT INTO ITEM (Name) VALUES ('Cupcakes
+')
+INSERT INTO ITEM (Name) VALUES ('Honey
+')
+INSERT INTO ITEM (Name) VALUES ('Mineral Water
+')
+INSERT INTO ITEM (Name) VALUES ('Carrotts
+')
+INSERT INTO ITEM (Name) VALUES ('Black Beans
+')
+INSERT INTO ITEM (Name) VALUES ('Rice
+')
+INSERT INTO ITEM (Name) VALUES ('Kidney Beans
+')
+INSERT INTO ITEM (Name) VALUES ('Peas
+')
+INSERT INTO ITEM (Name) VALUES ('Honey Mustard
+')
+INSERT INTO ITEM (Name) VALUES ('Taco Shells
+')
+INSERT INTO ITEM (Name) VALUES ('Whipped Cream
+')
+INSERT INTO ITEM (Name) VALUES ('Oranges
+')
+INSERT INTO ITEM (Name) VALUES ('Apples
+')
+INSERT INTO ITEM (Name) VALUES ('Tea
+')
+INSERT INTO ITEM (Name) VALUES ('Onion
+')
+INSERT INTO ITEM (Name) VALUES ('Jelly
+')
+INSERT INTO ITEM (Name) VALUES ('PeanutButter
+')
+INSERT INTO ITEM (Name) VALUES ('Tomato Sauce
+')
+INSERT INTO ITEM (Name) VALUES ('Coconut Oil
+')
+INSERT INTO ITEM (Name) VALUES ('Chicken
+')
+INSERT INTO ITEM (Name) VALUES ('Strawberries
+')
+INSERT INTO ITEM (Name) VALUES ('Cucumber
+')
+INSERT INTO ITEM (Name) VALUES ('Avocado
+')
+INSERT INTO ITEM (Name) VALUES ('Waffles
+')
+
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Apples','1', '1', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Taco Shells','1', '1', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Cucumber','1', '1', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Honey Mustard','1', '1', '5', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Whipped Cream','1', '2', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Avocado','1', '2', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Black Beans','1', '2', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Tomatoes','1', '2', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Broccoli','1', '2', '5', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Honey','1', '3', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Avocado','1', '3', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Cucumber','1', '3', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('That Loud gummi bites','1', '3', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Flour','1', '3', '5', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Onion','2', '1', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('That Loud gummi bites','2', '1', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Coffe Beans','2', '2', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Waffles','2', '2', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Soup','2', '3', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Onion','2', '3', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Jelly','3', '1', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Avocado','3', '1', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Apples','3', '1', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Cheese','3', '1', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Mineral Water','3', '2', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Black Beans','3', '2', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Broccoli','3', '2', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Tea','3', '2', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Mineral Water','3', '3', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Milk','3', '3', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Flour','3', '3', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Black Beans','3', '3', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Water','3', '4', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('That Loud gummi bites','3', '4', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Peas','3', '4', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Cucumber','3', '4', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Peas','3', '5', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Tomato Sauce','3', '5', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('That Loud gummi bites','3', '5', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Avocado','3', '5', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Bacon','3', '6', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Pasta','3', '6', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Pasta','3', '6', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Pasta','3', '6', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Oranges','3', '7', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Apples','3', '7', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Honey Mustard','3', '7', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Tomatoes','3', '7', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Onion','4', '1', '1', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Coffe Beans','4', '1', '2', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Cucumber','4', '1', '3', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Broccoli','4', '1', '4', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Tomatoes','4', '1', '5', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Kidney Beans','4', '1', '6', 'In the Middle','Next to thingy')
+INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('Apples','4', '1', '7', 'In the Middle','Next to thingy')
+
