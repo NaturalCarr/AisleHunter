3b6078c6c75497ec7041ae1ec6078173e20dc1f0
isayahc <isayah@DESKTOP-1E903EP.localdomain>
2018-12-21 17:48:59 -0500

    Cleaner code and added documentation

--- Files/AisleClass.py
+++ Files/AisleClass.py
@@ -5,54 +5,50 @@ class Store:
     """
     
     StoreID = 0
+
+    def __doc__(self):
+        """ The Purpose of this class is to input the information and formatr it based on the 
+        InsertTemplate.txt file. Store takes the arguments and puts it all into a 2D array.
+        The x value corresponds to the number of the Aisle. The y value corresponds to the 
+        position in the nth asile. This is put together and store into the list object RL
+        """
     def __init__(self, Name, ZipCode, x, y, Position =None, Content= None, RL=None ):
-        #print("hello")
         self.Name = Name 
         self.ZipCode = ZipCode
-        self.x = range(1,x) ##x cord in matrix Asile
+        self.x = range(1,x) ##x cord in matrix Aisle
         self.y = range(1,y) #y cord in matrix Postion
-        self.Position = Position
+        self.Position = Position #x,y
         self.Content = Content
-        self.RL = RL
+        self.RL = RL 
         Store.StoreID +=1
-        #print("Hello")
         RL = []
         
+        #creates a tuple object that holds the value for x,y
         if self.Position is None:
             self.Position = x,y
 
         if self.RL is None:
             self.RL = []
 
+        #turns the data from food.txt into a list of string objects
         if self.Content is None:
             with open ("food.txt") as f:
                 ReadFoodData = f.readlines()
                 self.Content = [x.strip() for x in ReadFoodData]
         
-
-
         for i in self.x:
             self.RL.append([])
-            print (RL)
-            print(i)
+
             for j in self.y:
                 print(j)
                 Store.r = random.randint(0,len(self.Content))
                 if self.Content[Store.r-1] == "":
-                    print("This is Store.r:", Store.r)
-                    Store.r = random.randint(0,len(self.Content))
-                    
+                    Store.r = random.randint(0,len(self.Content))                    
                 try:
                     self.RL[i-1].append(self.Content[Store.r])
                 except IndexError:
-                    print(self.RL)
-                    print(i, Store.r)
                     self.RL[i-2].append(self.Content[Store.r-1])
 
-                
-                print(self.RL)
-
-    #def NextTO(self, x,y):
     def Asile(self):
         return len(self.y)+1  
 
@@ -64,68 +60,22 @@ class Store:
         print(xy)
         return xy
 
-    #def NextTo
-
     def PrintAll(self):
         for i in self.x:
             for j in self.y:
                 print(j,i, " ",self.RL[i-1][j-1])
-    #('Rice', '1', '3', '6','Middle','It''s Below The Dishwashing Liquid');
+
     def INSERT(self, storenum):
-        #print(self.Name)
         listi = list()
         for i in self.x:
             for j in self.y:
-                #print("'",self.RL[i-1][j-1],"'", "'",storenum, "'", "'",i,"'","'", j, "In the Middle", "Next to thingy")
                 try:
-                    x = "INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('{}','{}', '{}', '{}', '{}','{}')".format(self.RL[i-1][j-1],storenum,i,j,"In the Middle", "Next to thingy")
-                    #print("INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('{}','{}', '{}', '{}', '{}','{}')".format(self.RL[i-1][j-1],storenum,i,j,"In the Middle", "Next to thingy"))
+                    x = "INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('{}','{}', '{}', '{}', '{}','{}')".format(self.RL[i-1][j-1],storenum,i,j,"In the Middle", "Next to thingy")                  
                     listi.append(x)
                 except IndexError:
                     try:
-                        print(i,j)
-                        #print("INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('{}','{}', '{}', '{}', '{}','{}')".format(self.RL[0][0],storenum,i,j,"In the Middle", "Next to thingy"))
                         x= "INSERT INTO POSITION (ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment) Values ('{}','{}', '{}', '{}', '{}','{}')".format(self.RL[0][0],storenum,i,j,"In the Middle", "Next to thingy")
                         listi.append(x)
                     finally:
                         print("error:\n", i,j)
         return listi
-
-                
-
-
-
-        
-        
-
-
-    
-'''
-Store1 =Store("Isayah", 11225, 4, 6)
-Store2 =Store("ZayWorld", 6606, 6, 7)
-StoresID = []
-StoresID.append(Store1)
-StoresID.append(Store2)
-print(Store1.Asile())
-print(Store1.AsilePosition())
-Store1.Coordinates()
-Store1.PrintAll()
-#Store1.INSERT(1)
-#Store2.INSERT(2)
-#Store1.PrintAisle()
-print(StoresID[:])
-
-
-
-
-    
-
-Store1 =Store("Isayah", 11225, 4, 6)
-Store2 =Store("Isayer", 11213, 3, 4)
-x =Store1
-y =Store2
-x.PrintAisle()
-y.PrintAisle()
-print(x.StoreID)
-#print(y.StoreID)
-'''
--- Files/SendToText.py
+++ Files/SendToText.py
@@ -4,44 +4,9 @@ import AisleClass
 #Creates a list with the format ITEM, StoreID, Aisle_Number, Shelf_Number, PIA, Comment
 #input = empty list
 #output = a random string for item, a random numder for store ID, Aisle_Number, PIA, Comment
-'''
-print("Hello")
-def Position(asile, EmptyList= []):
-    print("I work")
 
-    #parses data from food.txt into a variable
-    with open ("food.txt") as f:
-        ReadFoodData = f.readlines()
-        content = [x.strip() for x in ReadFoodData]
-
-    #parses data from insert.sql
-
-    Store1 =AisleClass.Store("Isayah", 11225, 4, 6)
-
-
-
-with open ("food.txt") as f:
-        ReadFoodData = f.readlines()
-        content = [x.strip() for x in ReadFoodData]
-'''
 
 AllStores = []
-Store1 =AisleClass.Store("Isayah", 11225, 4, 6)
-AllStores.append(Store1)
-'''Store2 =AisleClass.Store("Ibayah", 11225, 4, 6)
-Store3 =AisleClass.Store("Idayah", 11225, 3, 7)
-Store4 =AisleClass.Store("Ilayah", 11225, 8, 5)
-Store5 =AisleClass.Store("Ikayah", 11225, 3, 6)'''
-
-
-OneDLsit = range(5)
-'''
-for d in OneDLsit:
-        AllStores.append(
-        print(Store1)
-
-#def NumofStores(num):
-'''
 def RandomZip():
         listsize = range(0,5)
         Zip = []
@@ -62,18 +27,12 @@ RandomDump("Zack")
 RandomDump ("Johnies Store of food")
 RandomDump("Salee Sale")
 RandomDump("James Food")
-#print(x)
-#print(AllStores[1].Name)
+
 
 loop = range(len(AllStores))
 finallist = []
 for l in loop:
         finallist.append(AisleClass.Store.INSERT(AllStores[l],l))
-
-#print(finallist[range(len(finallist)),])
-        
-
-
 
 
     
